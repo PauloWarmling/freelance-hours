57bc9ab3c1f8b649671f2d27e4cd10ee13fb6299
PauloWarmling <paulowarmling12@gmail.com>
2024-10-09 08:39:43 -0300

    criação model users e projects

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'rating' => fake()->randomElement([1,2,3,4,5]),
+            'avatar' => 'https://avatar.iran.liara.run/public',
+        ];
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Project;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::factory()->count(200)->create();
+
+        User::query()->inRandomOrder()->limit(10)->get()->each(fn (User $u) => Project::factory()->create(['created_by' => $u->id]));
+        
+    }
+}
